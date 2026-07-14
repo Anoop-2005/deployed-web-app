4289157828d0574c77465ebc87fd77138f8fa883
anoop <chauhananoop216@gmail.com>
2026-07-14 14:44:08 +0530

    fix userlayout

--- frontend/src/Layouts/UserLayout.jsx
+++ frontend/src/Layouts/UserLayout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet, useLocation } from 'react-router-dom'
 import Navbar from '../Components/Navbar'
-import ChatbotWidget from "../components/Chatbot";
+
 
 export default function UserLayout() {
   
